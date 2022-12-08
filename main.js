91f03fbe3374ba63f6c28496c8df9669ecd88a6e
JunSuKim <12191747@inha.edu>
2022-12-08 17:19:05 +0900

    a

--- main.js
+++ main.js
@@ -1,18 +1,68 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');1
+
+function templateHTML(title, list, body) {
+    return `
+    <!doctype html>
+    <html>
+    <head>
+        <title>WEB1 - ${title}</title> 
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(filelist){
+    var list = '<ul>';
+    var i = 0;
+    while(i < filelist.length){
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+        i++;
+    }
+    list = list +'</ul>';
+    return list;
+}
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            fs.readdir('./data', function(error, filelist){
+                console.log(filelist);
+                var title = 'Welcome';
+                var description = "Hello, Node.js";
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+
+        else{
+            fs.readdir('./data', function(error, filelist){
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                    var title = queryData.id;
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+        }
     }
-    if(request.url == '/favicon.ico'){
+    else {
         response.writeHead(404);
-        response.end();
-        return;  return response.writeHead(404);
+        response.end('Not Found');
     }
-    response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
- 
 });
 app.listen(3000);
